642fa62737af6d1a93b230c284ac99f4b143c631
eternity6666 <1462928058@qq.com>
2018-07-22 16:09:42 +0800

    add some tests

---
+++ algorithm-competition/go-to-room/unit4/example/4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+int readchar();
+int readint(int c);
+int code[8][1 << 8];
+int readcodes();
+
+int main()
+{
+    while(readcodes(){
+        for (;;)
+        {
+        }
+    }
+}
