18468348d695926a192e388019a431c5f596edf9
eternity6666 <1462928058@qq.com>
2018-10-28 13:21:49 +0800

    181028

--- practive/2018fall/2ShortestPath/a.cpp
+++ practive/2018fall/2ShortestPath/a.cpp
@@ -1,81 +1,169 @@
 #include <bits/stdc++.h>
 #define fei(a, b) for(int i = a; i <= b; i++)
 #define fej(a, b) for(int j = a; j <= b; j++)
-#define fni(a, b) for(int i = a; i < b; i++)
-#define fnj(a, b) for(int j = a; j < b; j++)
+#define fek(a, b) for(int k = a; k <= b; k++)
 using namespace std;
 const int maxn = 1000 + 5;
+const int inf = 0x3fffffff;
 
-struct node{
-    int a, b, t;
-}farm[maxn];
+int g[maxn][maxn], n,m,x, d1[maxn], d2[maxn];
+bool v1[maxn], v2[maxn];
 
-int code[maxn][maxn];
-int N, M, X, ans, time;
-
+void Dijkstra1(int s);
+void Dijkstra2(int s);
 bool input();
-int max(int a, int b);
-int got_ans(int a, int b);
-void got_code();
-int min(int a, int b);
 
 int main()
 {
     freopen("in.txt", "r", stdin);
+
     while(input())
     {
-        fei(1, N)
-            ans = max(ans, got_ans(1, X));
+        Dijkstra1(x);
+        Dijkstra2(x);
+        int ans = 0;
+        fei(1, n)
+            if(ans < d1[i] + d2[i])
+                ans = d1[i] + d2[i];
         cout << ans << endl;
     }
     return 0;
 }
 
-
-void got_code()
+void Dijkstra1(int s)
 {
-    /*
-    fei(1, N)
-        fej(1, N)
-        {
-            time = 0;
-            dfs(i, j);
-            code[i][j] = min(code[i][j], time);
-        }
-    */
+    d1[s] = 0;
+    fei(1, n)
+    {
+        int u = -1, mi = inf;
+        fej(1, n)
+            if(!v1[j] && d1[j] < mi)
+                u = j, mi = d1[j];
+        if(u == -1) return ;
+        v1[u] = 1;
+        fej(1, n)
+            if(!v1[j] && g[j][u] != inf && d1[j] > d1[u] + g[j][u])
+                d1[j] = d1[u] + g[j][u];
+    }
+}
+
+void Dijkstra2(int s)
+{
+    d2[s] = 0;
+    fei(1, n)
+    {
+        int u = -1, mi = inf;
+        fej(1, n)
+            if(!v2[j] && d2[j] < mi)
+                u = j, mi = d2[j];
+        if(u == -1) return ;
+        v2[u] = 1;
+        fej(1, n)
+            if(!v2[j] && g[u][j] != inf && d2[j] > g[u][j] + d2[u])
+                d2[j] = g[u][j] + d2[u];
+    }
 }
 
 bool input()
 {
-    if(~scanf("%d %d %d", &N, &M, &S))
+    if(~scanf("%d %d %d", &n, &m, &x))
     {
-        ans = 0;
-        fei(1, N)
-            fej(1, N)
-                code[i][j] = maxn;
-        fei(1, M)
+        fei(1, n)
         {
-            cin >> farm[i].a >> farm[i].b >> farm[i].t;
-            code[farm[i].a][farm[i].b] = farm[i].t;
+            v1[i] = v2[i] = 0;
+            d1[i] = d2[i] = inf;
+            fej(1, n)
+                g[i][j] = inf;
         }
-        got_code();
+        fei(1, m)
+        {
+            int a, b, t;
+            scanf("%d %d %d", &a, &b, &t);
+            if(g[a][b] > t)
+                g[a][b] = t;
+        }
         return true;
     }
     return false;
 }
 
-int min(int a, int b)
-{
-    return a < b ? a : b;
-}
+// #include <bits/stdc++.h>
+// #define fei(a, b) for (int i = a; i <= b; i++)
+// #define fej(a, b) for (int j = a; j <= b; j++)
+// #define fni(a, b) for (int i = a; i < b; i++)
+// #define fnj(a, b) for (int j = a; j < b; j++)
+// using namespace std;
+// const int maxn = 1000 + 5;
 
-int got_ans(int a, int b)
-{
-    return code[a][b] + code[b][a];
-}
+// struct node
+// {
+//     int a, b, t;
+// } farm[maxn];
 
-int max(int a, int b)
-{
-    return a > b ? a : b;
-}
+// int code[maxn][maxn];
+// int N, M, X, ans, time;
 
+// bool input();
+// int max(int a, int b);
+// int got_ans(int a, int b);
+// void got_code();
+// int min(int a, int b);
+
+// int main()
+// {
+//     freopen("in.txt", "r", stdin);
+//     while (input())
+//     {
+//         fei(1, N)
+//             ans = max(ans, got_ans(1, X));
+//         cout << ans << endl;
+//     }
+//     return 0;
+// }
+
+// void got_code()
+// {
+//     /*
+//     fei(1, N)
+//         fej(1, N)
+//         {
+//             time = 0;
+//             dfs(i, j);
+//             code[i][j] = min(code[i][j], time);
+//         }
+//     */
+// }
+
+// bool input()
+// {
+//     if (~scanf("%d %d %d", &N, &M, &S))
+//     {
+//         ans = 0;
+//         fei(1, N)
+//             fej(1, N)
+//                 code[i][j] = maxn;
+//         fei(1, M)
+//         {
+//             cin >> farm[i].a >> farm[i].b >> farm[i].t;
+//             code[farm[i].a][farm[i].b] = farm[i].t;
+//         }
+//         got_code();
+//         return true;
+//     }
+//     return false;
+// }
+
+// int min(int a, int b)
+// {
+//     return a < b ? a : b;
+// }
+
+// int got_ans(int a, int b)
+// {
+//     return code[a][b] + code[b][a];
+// }
+
+// int max(int a, int b)
+// {
+//     return a > b ? a : b;
+// }
--- practive/2018fall/2ShortestPath/j.cpp
+++ practive/2018fall/2ShortestPath/j.cpp
@@ -1,15 +1,189 @@
 #include <bits/stdc++.h>
 #define fei(a, b) for(int i = a; i <= b; i++)
 #define fej(a, b) for(int j = a; j <= b; j++)
+#define fek(a, b) for(int k = a; k <= b; k++)
+using namespace std;
+const int maxn = 1000 + 5;
+const int inf = 0x3fffffff;
+const int maxm = 10000 + 5;
+int N,M,S,T,G[maxn][maxn],d[maxn];
+bool v[maxn];
+
+void Dijkstra();
+bool input();
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    while(input())
+    {
+        Dijkstra();
+        cout << d[T] << endl;
+    }
+    return 0;
+}
+
+void Dijkstra()
+{
+    d[S] = 0;
+    fei(1, N)
+    {
+        int u = -1, MIN = inf;
+        fej(1, N)
+        {
+            if(d[j] < MIN && !v[j])
+            {
+                u = j;
+                MIN = d[j];
+            }
+        }
+        if(u == -1)
+            return ;
+        v[u] = 1;
+        fej(1, N)
+        {
+            if(!v[j] && G[u][j] != inf && G[u][j] + d[u] < d[j])
+                d[j] = d[u] + G[u][j];
+        }
+    }
+}
+
+bool input()
+{
+    if(~scanf("%d %d %d %d", &N, &M, &S, &T))
+    {
+        fei(1, N)
+        {
+            v[i] = 0;
+            d[i] = inf;
+            fej(1, N)
+                G[i][j] = inf;
+        }
+
+        fei(1, M)
+        {
+            int u, v, l;
+            scanf("%d %d %d", &u, &v, &l);
+            if(G[u][v] > l)
+                G[u][v] = G[v][u] = l;
+        }
+        return true;
+    }
+    return false;
+}
+
+/*
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <set>
+#define fei(a, b) for(int i = a; i <= b; i++)
+#define fej(a, b) for(int j = a; j <= b; j++)
+#define fni(a, b) for(int i = a; i < b; i++)
+#define fnj(a, b) for(int j = a; j < b; j++)
+using namespace std;
+const int maxn = 1000 + 5;
+
+int road[maxn][maxn];
+int N, M, S, T, ans;
+int d[maxn];
+bool v[maxn];
+
+bool input();
+void got_ans();
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+    while(input())
+    {
+        got_ans();
+        ans = d[T];
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+void got_ans()
+{
+    set<int> s;
+    set<int>::iterator it;
+    s.insert(S);
+    v[S] = 1;
+    while(!s.empty())
+    {
+        int mins, minl = maxn;
+        for(it = s.begin(); it != s.end(); it++)
+        {
+            int tmps, tmpl, tmp = *it;
+            tmpl = maxn;
+            // cout << tmp << endl;
+            fei(1, N)
+            {
+                if(road[tmp][i] == maxn || d[i] != maxn)
+                    continue;
+                else
+                {
+                    if(road[tmp][i] <= tmpl)
+                    {
+                        tmpl = road[tmp][i];
+                        tmps = i;
+                    }
+                }
+            }
+            if(minl >= d[tmp] + tmpl)
+            {
+                mins = tmps;
+                minl = tmpl + d[tmp];
+            }
+        }
+        s.insert(mins);
+        d[mins] = minl;
+        if(mins == T)
+            return ;
+    }
+    
+}
+
+bool input()
+{
+    if(~scanf("%d %d %d %d", &N, &M, &S, &T))
+    {
+        ans = 0;
+        int u,v,l;
+        fei(1, N)
+        {   
+            d[i] = maxn;
+            fej(1, N)
+                road[i][j] = maxn;
+        }
+        d[S] = 0;
+        fei(1, M)
+        {
+            cin >> u >> v >> l;
+            if(l < road[u][v])
+                road[u][v] = l;
+            if(l < road[v][u])
+                road[v][u] = l;
+        }
+        return true;
+    }
+    return false;
+}
+*/
+
+/*
+#include <bits/stdc++.h>
+#define fei(a, b) for(int i = a; i <= b; i++)
+#define fej(a, b) for(int j = a; j <= b; j++)
 using namespace std;
 const int maxn = 1000 + 5;
 const int maxm = 10000 + 5;
 
-/*
 struct edge{
     int u, v, l;
 }es[maxm];
-*/
 
 int road[maxn][maxn];
 int d[maxn];
@@ -94,3 +268,4 @@ bool input()
     }
     return false;
 }
+*/
