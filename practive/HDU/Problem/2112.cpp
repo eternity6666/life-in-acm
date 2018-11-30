2ba4f03bdba60201fde0299a8b5a890947d9b9df
eternity6666 <1462928058@qq.com>
2018-11-30 21:50:54 +0800

    updated

--- practive/HDU/Problem/2112.cpp
+++ practive/HDU/Problem/2112.cpp
@@ -1,16 +1,22 @@
 #include <bits/stdc++.h>
 #define fei(a, b) for(int i = a; i <= b; i++)
 #define fej(a, b) for(int j = a; j <= b; j++)
-#define fek(a, b) for(int k = a; k <= b; k++)
+#define fni(a, b) for(int i = a; i < b; i++)
+#define fnj(a, b) for(int j = a; j < b; j++)
 using namespace std;
+
 const int maxn = 100 + 5;
 
+int n;
+string b, e;
 int main()
 {
     freopen("in.txt", "r", stdin);
-
-    while(input())
+    while(~scanf("%d", &n))
     {
+        if(n == -1)
+            break;
+        cin >> b >> e;
 
     }
     return 0;
