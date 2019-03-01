07fac0f1a8566fbfc3a705592ff2b269cd07b8ef
eternity6666 <1462928058@qq.com>
2019-03-01 22:05:02 +0800

    updated

---
+++ practive/NowCoder/190301/error/a.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define fei(a, b) for(int i = a; i <= b; i++)
+#define fej(a, b) for(int j = a; j <= b; j++)
+#define fek(a, b) for(int k = a; k <= b; k++)
+using namespace std;
+const int maxn = 10e9 + 5;
+
+long long n, m;
+int v[maxn];
+
+bool got_ans()
+{
+    fei(0, n)
+        v[i] = i;
+    long long tmp = n;
+
+
+
+}
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n >> m;
+        bool flag;
+        if(n == m)
+            flag = 1;
+        else if
+        {
+            falg = got_ans();
+        }
+        else
+            flag = 0;
+
+        if(flag)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
+
