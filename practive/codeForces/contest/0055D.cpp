c66b67b65f50a7e06b08b697051c887908d418c5
eternity6666 <1462928058@qq.com>
2019-09-12 22:36:11 +0800

    updated

---
+++ practive/codeForces/contest/0055D.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[10][18];
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int l, r;
+        cin >> l >> r;
+        
+    }
+    return 0;
+}
