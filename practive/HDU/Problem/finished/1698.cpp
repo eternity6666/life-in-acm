0bc68bcbfc32096e855b83115f9e2ce4f41de97a
eternity6666 <1462928058@qq.com>
2019-09-16 10:24:59 +0800

    hdu 1698.cpp - Just a Hook

--- practive/HDU/Problem/finished/1698.cpp
+++ practive/HDU/Problem/finished/1698.cpp
@@ -1,3 +1,4 @@
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -73,5 +74,71 @@ int main()
         printf("Case %d: The total value of the hook is %d.\n", i, query(1, 1, n, 1, n));
     }
 
+    return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 1e5 + 10;
+
+int lazy[maxn << 2];
+
+void pushDOWN(int id)
+{
+    if (lazy[id])
+    {
+        lazy[id << 1] = lazy[id << 1 | 1] = lazy[id];
+        lazy[id] = 0;
+    }
+}
+
+void update(int id, int l, int r, int ul, int ur, int val)
+{
+    if (ul <= l && r <= ur)
+    {
+        lazy[id] = val;
+        return;
+    }
+    int m = (l + r) >> 1;
+    pushDOWN(id);
+    if (ul <= m)
+        update(id << 1, l, m, ul, ur, val);
+    if (ur > m)
+        update(id << 1 | 1, m + 1, r, ul, ur, val);
+}
+
+int query(int id, int l, int r, int ql, int qr)
+{
+    if (ql <= l && r <= qr && lazy[id] > 0)
+        return lazy[id] * (r - l + 1);
+    int m = (l + r) >> 1;
+    int sum = 0;
+    if (ql <= m)
+        sum += query(id << 1, l, m, ql, qr);
+    if (qr > m)
+        sum += query(id << 1 | 1, m + 1, r, ql, qr);
+    return sum;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie();
+    cout.tie();
+    int n, q, t;
+    cin >> t;
+    for (int i = 1; i <= t; ++i)
+    {
+        cin >> n >> q;
+        update(1, 1, n, 1, n, 1);
+        while (q--)
+        {
+            int x, y, z;
+            cin >> x >> y >> z;
+            update(1, 1, n, x, y, z);
+        }
+        printf("Case %d: The total value of the hook is %d.\n", i, query(1, 1, n, 1, n));
+    }
     return 0;
 }
