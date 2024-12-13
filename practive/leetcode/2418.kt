9dffd6b5a478cc28b07e5681495e88b221c086d7
eternity6666 <eternity6666@qq.com>
2024-12-14 01:39:27 +0800

    feat: leetcode-2418

---
+++ practive/leetcode/2418.kt
@@ -0,0 +1,9 @@
+package leetcode;
+
+class Solution {
+    fun sortPeople(names: Array<String>, heights: IntArray): Array<String> {
+        val result =  Array<String>(names)
+        val n = result.size
+
+    }
+}
